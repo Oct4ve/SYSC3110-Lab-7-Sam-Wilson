4303a2622f8b9fedf614e665ffc5f5e55872625b
samwilson3 <samwilson292929@gmail.com>
2023-11-14 18:04:52 -0500

    Tax rate bug fix

--- src/Site.java
+++ src/Site.java
@@ -2,7 +2,7 @@ abstract class Site {
     public String siteName;
     protected int _units;
     protected double _rate;
-    public static double TAX_RATE = 1.13;
+    public static double TAX_RATE = 0.13;
     public double getBillableAmount(){
         return getBaseAmount() + getTaxAmount();
     }
